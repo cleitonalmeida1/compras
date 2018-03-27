212546326481325acdc7367d949e96e07116c602
Cleiton Gonçalves de Almeida <cgalmeida@azi.com.br>
2018-03-26 22:13:40 -0400

    Projeto Criado; Criação da Entidade Categoria; Conexão com o Banco de Dados.

---
+++ src/main/java/br/com/analise/compras/Entity/Categoria.java
@@ -0,0 +1,36 @@
+package br.com.analise.compras.Entity;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_CATEGORIA")
+public class Categoria {
+
+    @Id
+    @Column(name = "CA_ID")
+    private Integer id;
+
+    @Column(name = "CA_NOME")
+    private String nome;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
